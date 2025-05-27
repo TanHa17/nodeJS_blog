f265860d4b2adeed8bfb54663a58d881992a5144
TanHa17 <ha.tan@tiahgroup.com>
2025-05-27 23:30:01 +0700

    MVC

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 3001
+
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 //HTTP logger
@@ -16,13 +19,9 @@ app.engine('.hbs', handlebars.engine({
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+route(app);
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
