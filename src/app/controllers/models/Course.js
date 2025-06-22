eb99e8558d0c92f9b8076410bdc6b3fb4310fab6
TanHa17 <hatan61cdt1@gmail.com>
2025-06-22 22:04:49 +0700

    22062025

--- src/app/controllers/models/Course.js
+++ src/app/controllers/models/Course.js
@@ -1,14 +1,34 @@
 const mongoose = require('mongoose');
+const slugUpdater = require('mongoose-slug-updater');
+const mongoose_delete = require('mongoose-delete');
+
+mongoose.plugin(slugUpdater);
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const Courses = new Schema({
-    name: { type: String, maxLength: 255 },
-    description: { type: String, maxLength: 600 },
-    image: { type: String, maxLength: 255 },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
+const Courses = new Schema(
+    {
+        name: { type: String, required: true },
+        description: { type: String, maxLength: 600 },
+        image: { type: String, maxLength: 255 },
+        slug: { type: String, slug: 'name', unique: true },
+        videoId: { type: String, required: true },
+        level: { type: String, maxLength: 255 },
+    },
+    {
+        timestamps: true,
+    },
+);
+Courses.plugin(mongoose_delete, {
+    deletedAt: true,
+    overrideMethods: [
+        'find',
+        'findOne',
+        'findOneAndUpdate',
+        'update',
+        'updateOne',
+        'updateMany',
+    ],
 });
-
 module.exports = mongoose.model('Courses', Courses);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const methodOverride = require('method-override');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const path = require('path');
@@ -11,16 +12,25 @@ db.connect();
 const app = express();
 const port = 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Body parsing middleware should come before static files
+app.use(express.json()); // Để parse body dạng JSON
+app.use(express.urlencoded({ extended: true })); // Để parse form-data (application/x-www-form-urlencoded)
 
+app.use(express.static(path.join(__dirname, 'public')));
 //HTTP logger
 app.use(morgan('combined'));
+
+//Method override
+app.use(methodOverride('_method'));
 
 //Template engine
 app.engine(
     '.hbs',
     handlebars.engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     }),
 );
 app.set('view engine', '.hbs');
